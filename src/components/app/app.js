3f649b4a04735a5608c3a193eecb51a2cf4b60fa
Max <maxpanas008@gmail.com>
2022-02-16 11:54:00 +0100

    First commit. Some changes were provided to app

---
+++ src/components/app/app.js
@@ -0,0 +1,54 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John S.', salary: 800, increase: true, id: 1},
+                {name: 'Tom H.', salary: 3000, increase: false, id: 2},
+                {name: 'Jack D.', salary: 7000, increase: true, id: 3}
+            ]
+        }
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                />
+                <EmployeesAddForm/>
+            </div>
+        );
+    }
+}
+
+
+export default App;
